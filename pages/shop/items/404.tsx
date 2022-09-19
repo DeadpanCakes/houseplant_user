12258e3a99527d699806d408a34d23f41dc72e8b
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-19 15:20:58 -0700

    made basic item 404 page

---
+++ pages/shop/items/404.tsx
@@ -0,0 +1,13 @@
+const Item404 = () => {
+  return (
+    <div>
+      <h1>This Item Does Not Exist</h1>
+      <p>
+        It's possible that this item is currently out of stock or is being sold
+        by another name. Please try searching for it elsewhere.
+      </p>
+    </div>
+  );
+};
+
+export default Item404;
