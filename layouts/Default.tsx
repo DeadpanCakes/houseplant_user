8e21eaa81a456baf3718d1e8ae94847617163b75
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-19 14:20:17 -0700

    made default layout to make header and footer reusable

---
+++ layouts/Default.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const Default = ({ children }) => {
+  return (
+    <>
+      <header>
+        <h1>Home</h1>
+        <nav>
+          <Link href="/register">
+            <a>Sign Up</a>
+          </Link>
+          <Link href="/log-in">
+            <a>Log In</a>
+          </Link>
+        </nav>
+      </header>
+      {children}
+      <footer>
+        <p>Copyright 2022</p>
+      </footer>
+    </>
+  );
+};
+
+export default Default;
