b9b06cdd151d0e77c7bbb8e248661dbe2c1bf72c
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-21 14:14:52 -0700

    made component to control filters

---
+++ components/FilterControl.tsx
@@ -0,0 +1,35 @@
+const FilterControl = ({ tags, changeHandler }) => {
+  const removeTag = (target, tags) => {
+    return tags.filter((tag) => tag._id !== target._id);
+  };
+  const addTag = (newTag, tags) => {
+    return tags.concat(newTag);
+  };
+
+  return (
+    <ul>
+      {tags.map((tag) => {
+        return (
+          <li>
+            <span>
+              <input
+                id={tag._id}
+                type="checkbox"
+                onChange={() => {
+                  changeHandler((prevState) => {
+                    if (prevState.find((t) => t._id === tag._id)) {
+                      return removeTag(tag, prevState);
+                    }
+                    return addTag(tag, prevState);
+                  });
+                }}
+              />
+              <label htmlFor={tag._id}>{tag.name}</label>
+            </span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+export default FilterControl;
--- pages/shop/categories/[id].tsx
+++ pages/shop/categories/[id].tsx
@@ -1,15 +1,53 @@
+import { useState } from "react";
 import ProductListing from "../../../components/ProductListing";
+import Sorter from "../../../components/Sorter";
+import FilterControl from "../../../components/FilterControl";
 
 const Category = (props) => {
   const category = JSON.parse(props.category);
   const products = JSON.parse(props.products);
+  const [sortCritera, setSortCriteria] = useState("default");
+  const [desiredTags, setDesiredTags] = useState([]);
+
+  const sortHandler = (a, b) => {
+    if (sortCritera !== "default") {
+      return a[sortCritera] - b[sortCritera];
+    }
+    return a["_id"] - b["_id"];
+  };
+
+  const filterHandler = (product) =>
+    desiredTags.length > 1
+      ? product.categories.find((category) =>
+          desiredTags.includes(category.name)
+        )
+      : product;
   return (
     <>
       <h1>{category.name}</h1>
+      <Sorter
+        criteria={["price", "name", "default", "discount"]}
+        changeHandler={setSortCriteria}
+      />
+      <FilterControl
+        tags={[
+          { _id: 4, name: "tropical" },
+          { _id: 5, name: "succulent" },
+        ]}
+        changeHandler={setDesiredTags}
+      />
       <ul>
-        {products.map((product) => {
-          return <ProductListing key={product._id} product={product} />;
-        })}
+        {products
+          .sort(sortHandler)
+          .filter(filterHandler)
+          .map((product) => {
+            return <ProductListing key={product._id} product={product} />;
+          })}
+      </ul>
+      <ul>
+        {desiredTags.map((t) => (
+          <li>{t.name}</li>
+        ))}
       </ul>
     </>
   );
@@ -28,7 +66,43 @@ export const getServerSideProps = (context) => {
       description: "This is a placeholder object",
       stock: 10,
       price: 10,
+      discount: 15,
+      categories: [
+        {
+          _id: 1,
+          name: "Tropical",
+          description:
+            "Leafy Plants That Can Endure Periods Of Shade; Great Indoor Plants",
+        },
+        { _id: 2, name: "Pink", description: "Plants With Shades Of Pink" },
+      ],
+      isPublished: true,
+    },
+    {
+      _id: 26,
+      name: "Product 2",
+      description: "This is a placeholder object",
+      stock: 10,
+      price: 15,
       discount: 25,
+      categories: [
+        {
+          _id: 1,
+          name: "Tropical",
+          description:
+            "Leafy Plants That Can Endure Periods Of Shade; Great Indoor Plants",
+        },
+        { _id: 2, name: "Pink", description: "Plants With Shades Of Pink" },
+      ],
+      isPublished: true,
+    },
+    {
+      _id: 28,
+      name: "Product 3",
+      description: "This is a placeholder object",
+      stock: 5,
+      price: 5,
+      discount: 0,
       categories: [
         {
           _id: 1,
