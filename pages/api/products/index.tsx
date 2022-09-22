b1724069607744527a9339ee960c6d2e02d3f887
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-09-22 16:44:42 -0700

    replaced placeholder with proper api call

--- pages/api/products/index.tsx
+++ pages/api/products/index.tsx
@@ -1,72 +1,18 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 const handler = (req, res) => {
   switch (req.method) {
     case "GET":
       return get(req, res);
     default:
-      return res.json({ message: "Unsupported HTTP Method" });
+      return res.status(400).json({ message: "Unsupported HTTP Method" });
   }
-  res.status(200).json({ name: "John Doe" });
 };
 
-const get = (req, res) => {
-  const products = [
-    {
-      _id: "25",
-      name: "Product",
-      description: "This is a placeholder object",
-      stock: 10,
-      price: 10,
-      discount: 15,
-      categories: [
-        {
-          _id: 1,
-          name: "Tropical",
-          description:
-            "Leafy Plants That Can Endure Periods Of Shade; Great Indoor Plants",
-        },
-        { _id: 2, name: "Pink", description: "Plants With Shades Of Pink" },
-      ],
-      isPublished: true,
-    },
-    {
-      _id: "26",
-      name: "Product 2",
-      description: "This is a placeholder object",
-      stock: 10,
-      price: 15,
-      discount: 25,
-      categories: [
-        {
-          _id: 1,
-          name: "Tropical",
-          description:
-            "Leafy Plants That Can Endure Periods Of Shade; Great Indoor Plants",
-        },
-        { _id: 2, name: "Pink", description: "Plants With Shades Of Pink" },
-      ],
-      isPublished: true,
-    },
-    {
-      _id: "28",
-      name: "Product 3",
-      description: "This is a placeholder object",
-      stock: 5,
-      price: 5,
-      discount: 0,
-      categories: [
-        {
-          _id: 1,
-          name: "Tropical",
-          description:
-            "Leafy Plants That Can Endure Periods Of Shade; Great Indoor Plants",
-        },
-        { _id: 2, name: "Pink", description: "Plants With Shades Of Pink" },
-      ],
-      isPublished: true,
-    },
-  ];
+const get = async (req, res) => {
+  const DEV_API = process.env.DEV_API;
+  const products = await fetch(
+    DEV_API || process.env.PROD_API + "/products"
+  ).then((res) => res.json());
+  console.log(products);
   res.json(products);
 };
 
